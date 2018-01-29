3e6a4c437b95e157627ee27b5ab68442b121acdb
huangxiaotu <549435892@qq.com>
2018-01-29 21:40:43 +0800

    #清除文件

--- homework/HuangJing/src/validators/mobileValidator.ts
+++
@@ -1,9 +0,0 @@
-// 电话校验方法
-import {FormControl} from "@angular/forms";
-
-export function validator_mobile(control: FormControl): any {
-  let mobileValue = (control.value || '') + '';
-  let rege =  /^1[34578][0-9]\d{8}$/;
-  let isMobile = rege.test(mobileValue);
-  return isMobile ? null : {'mobile': true};
-}
